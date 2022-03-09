5232b860cb39e77e4f60266742ac148ae57a7f8d
Erin Vasquez <erin.vasquez@gmail.com>
2022-03-09 15:11:19 -0800

    Initial commit
    General program that should do nothing

---
+++ testprogram1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dminor {
+
+    class testprogram1.cs {
+
+        static void Main(string[] args) {
+
+        return 0;
+        }
+    }
+
+}
